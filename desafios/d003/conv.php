4e03aae556d43a776d353be79b4d926d40c745dd
Eduardo de Oliveira Marques <98243709+EduardoDuduu@users.noreply.github.com>
2023-04-04 11:56:18 -0300

    Conversor de moedas simples
    Desafio 03, top de mais, criei um conversor de moedas com o PHP, e adicionei uma formatação usando o intl, tornando o código mais elaborado e dinâmico.

--- desafios/d003/conv.php
+++ desafios/d003/conv.php
@@ -5,23 +5,33 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Conversor de moedas</title>
-
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <main>
         <h1>Conversor de Moedas v1.0</h1>
         <section>
-            <?php 
-                // $_cot = 5.06;
-                // $_valCart = (float) $_GET["convNum"];
-                // $_formatBr = number_format($_valCart, 2, ',', '.');
-                // $_formatUs = $_valCart / $_cot;
+            <?php
+                // Cotação copiada do Google
+                $cotação = 5.06;
 
-                // echo "<p>Seus R\$$_formatBr equivalem a, </p>";
+                // Quanto $$ você tem?
+                $real = $_GET["convNum"];
 
-                
+                // Equivalência em dólar
+                $dólar = $real / $cotação;
+
+                // Mostrar o resultado
+                // echo "Seus R\$" . number_format($real, 2, ',', '.') . " equivalem a US\$" . number_format($dólar, 2, ',', '.');
+
+                // Formatação de moedas com internacionalização!
+                $padrão = numfmt_create("pt-BR", NumberFormatter::CURRENCY);
+
+                echo "<p>Seus " . numfmt_format_currency($padrão, $real, "BRL") . " equivalem a <strong>" . numfmt_format_currency($padrão, $dólar, "USD") . "</strong></p>";
             ?>
+            <p>Cotação fixa em <strong>R$5,06</strong></p>
         </section>
+        <a href="javascript:history.go(-1)">Voltar para o início</a>
     </main>
 </body>
 </html>
