3ada6bda28d8e36d2a67d43c4efe90e8ef3448ef
Eduardo de Oliveira Marques <98243709+EduardoDuduu@users.noreply.github.com>
2023-04-04 09:58:18 -0300

    Update desafio

---
+++ desafios/d003/conv.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Conversor de moedas</title>
+
+</head>
+<body>
+    <main>
+        <h1>Conversor de Moedas v1.0</h1>
+        <section>
+            <?php 
+                // $_cot = 5.06;
+                // $_valCart = (float) $_GET["convNum"];
+                // $_formatBr = number_format($_valCart, 2, ',', '.');
+                // $_formatUs = $_valCart / $_cot;
+
+                // echo "<p>Seus R\$$_formatBr equivalem a, </p>";
+
+                
+            ?>
+        </section>
+    </main>
+</body>
+</html>
